1fc39d89d315af79bd39c5dba068880196ccc208
Jonathan Barorot <jkimutai005@gmail.com>
2026-07-18 00:37:29 +0300

    extra changes to logic

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function createLoginTracker(userInfo, maxAttempts) {
     let attemptCount = 0;
-
+    
     return (passwordAttempt) => {
         attemptCount++;
         
         if (attemptCount > maxAttempts) {
-            return "Account is restricted due to too many attempts";
+            return "Account is restricted due to too many failed attempts";
         }
 
         if (passwordAttempt === userInfo.password) {
@@ -23,6 +23,8 @@ const userInfo = {
 
 const login = createLoginTracker(userInfo, 3);
 
+console.log(login("password123"))
+console.log(login("wrongpass"));
 console.log(login("wrongpass"));
 console.log(login("wrongpass"));
 console.log(login("wrongpass"));
