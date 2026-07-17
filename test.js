04fd78df065f2ad037a34d1ed877c03819a3197e
Jonathan Barorot <jkimutai005@gmail.com>
2026-07-18 00:16:16 +0300

    update userinfo

---
+++ test.js
@@ -0,0 +1,32 @@
+
+function createLoginTracker(userInfo, maxAttempts) {
+    let attemptCount = 0;
+
+    return (passwordAttempt) => {
+        attemptCount++;
+        
+        if (attemptCount > maxAttempts) {
+            return "Account is restricted due to too many attempts";
+        }
+
+        if (passwordAttempt === userInfo.password) {
+            return "Login Successful";
+        } else {
+            return "Incorrect password. Attempts remaining: " + (maxAttempts - attemptCount);
+        }
+    };
+}
+
+
+const userInfo = {
+    "username": "user1",
+    "password": "password123"
+};
+
+
+const login = createLoginTracker(userInfo, 3);
+
+console.log(login("wrongpass"));
+console.log(login("wrongpass"))
+console.log(login("wrongpass"))
+console.log(login("password123"))
